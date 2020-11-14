50cd00b6bfad32c283a9752f084398965351ae91
sweetapple94 <sweetapple94@naver.com>
2020-11-14 15:27:29 +0900

    11.14
    넘나 힘들어 담에할래...

---
+++ Char/src/Group_word_하기싫어.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Group_word {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		String s;
+		int index = 0;
+		
+		for(int i = 0; i<num; i ++)
+		{
+			s = sc.next();
+			
+			char [] word = new char [s.length()];
+			
+			for(int j =0; j < word.length; j ++)
+			{
+				s.charAt(i)
+					
+			}
+		}
+	}
+}
